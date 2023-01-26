296cc7963d60dcf842fe5b998f3e8cd9afbf7735
Matheus Marcelino <110202966+Matheus-Marcelino@users.noreply.github.com>
2023-01-26 14:27:08 -0300

    Definindo as configurações da janela

--- main.py
+++ main.py
@@ -31,3 +31,20 @@ class App(object):
                       'porfavor restaure ela!')
             self.__WINDOW.destroy()
         self.__WINDOW.mainloop()
+
+    def __screen_settings(self) -> None:
+        """Contém todas as definições da tela"""
+        self.__WINDOW["bg"] = self.__COLOR_BASIC
+        self.__WINDOW.resizable(width=FALSE, height=FALSE)
+        self.__WINDOW.title('Gerador de Qr Code')
+
+        # Centralizando a janela no monitor
+        HEIGHT = int(400)
+        WIDTH = int(500)
+        X: int = self.__WINDOW.winfo_screenwidth() // 2 - (WIDTH + 2) // 2
+        Y: int = self.__WINDOW.winfo_screenheight() // 2 - HEIGHT // 2
+        self.__WINDOW.geometry(f'{WIDTH}x{HEIGHT}+{X}+{Y}')
+
+        # Carregando icon da janela
+        self.__WINDOW.call('wm', 'iconphoto', self.__WINDOW._w,
+                           PhotoImage(file='icon/qr.png'))
